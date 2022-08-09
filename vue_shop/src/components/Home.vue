db6556cc51e9c6de03564369e1a59e9a8becf7b6
zk-wl <wl2521440180@163.com>
2022-08-09 19:58:41 +0800

    完成登录功能

---
+++ vue_shop/src/components/Home.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <el-button typr="info" @click="logout">退出</el-button>
+    </div>
+</template>
+
+<script>
+export default {
+    data () {
+        return 
+    },
+    methods: {
+        logout () {
+            window.sessionStorage.clear()
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>

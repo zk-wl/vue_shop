3e898114437a33a4596a673199c2dc7d62de7eb5
zk-wl <wl2521440180@163.com>
2022-08-18 01:44:36 +0800

    初始化项目

---
+++ vue_shop/src/components/Home.vue
@@ -0,0 +1,30 @@
+<template>
+    <div>
+        <el-button typr="info" @click="logout">退出</el-button>
+    </div>
+    <el-container>
+  <el-header>Header</el-header>
+  <el-container>
+    <el-aside width="200px">Aside</el-aside>
+    <el-main>Main</el-main>
+  </el-container>
+</el-container>
+</template>
+
+<script>
+export default {
+    data () {
+        return 
+    },
+    methods: {
+        logout () {
+            window.sessionStorage.clear()
+            this.$router.push('/login')
+        }
+    }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
--- vue_shop/src/plugins/element.js
+++ vue_shop/src/plugins/element.js
@@ -1,4 +1,4 @@
 import Vue from 'vue'
-import { Button } from 'element-ui'
+import { Button , Form ,FormItem ,  } from 'element-ui'
 
 Vue.use(Button)
